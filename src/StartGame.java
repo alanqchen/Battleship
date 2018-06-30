14bda9945cf3e7b4b7a2daf27c4ad696e0378541
nodinawe <alanchen1908@gmail.com>
2018-06-29 23:51:37 -0400

    Create StartGame.java

---
+++ src/StartGame.java
@@ -0,0 +1,14 @@
+/**
+ * @author Alan Chen
+ * AP Computer Science 2016-2017
+ * Project: Battleship (final Graphics Project)
+ * TODO: 
+ */
+public class StartGame { // main class, starts up menu and game
+
+	public static void main(String[] args) {
+		new MainMenuFrame(); // initiates new Menu
+
+	}
+
+}
